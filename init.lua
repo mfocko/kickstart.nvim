e0f425a03a275004f4fef09fdea01a8a38f5feb9
Matej Focko <me@mfocko.xyz>
2024-07-01 18:55:35 +0200

    feat: add themes
    Signed-off-by: Matej Focko <me@mfocko.xyz>

--- init.lua
+++ init.lua
@@ -803,21 +803,44 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  { -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
     'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      ColorMyPencils 'tokyonight-night'
+    end,
+  },
 
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+  {
+    'nyoom-engineering/oxocarbon.nvim',
+    priority = 1000,
+    config = function()
+      -- ColorMyPencils 'oxocarbon'
+    end,
+  },
+
+  {
+    'Mofiqul/dracula.nvim',
+    priority = 1000,
+    config = function()
+      -- ColorMyPencils 'dracula'
+    end,
+  },
+
+  {
+    'maxmx03/solarized.nvim',
+    priority = 1000,
+    config = function()
+      -- vim.cmd.colorscheme 'solarized'
+      -- ColorMyPencils 'solarized'
+    end,
+  },
+
+  {
+    'olimorris/onedarkpro.nvim',
+    priority = 1000,
+    config = function()
+      -- ColorMyPencils 'onedark'
     end,
   },
 
